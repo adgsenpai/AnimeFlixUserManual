84b4e7435039b54741d9b2e5b5f02747e731a361
Ashlin Darius Govindasamy <adg@adgstudios.co.za>
2024-01-29 19:46:55 +0200

    first commit

---
+++ app.config.ts
@@ -0,0 +1,38 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Animeflix',
+    description: 'Animeflix is a free anime streaming website where you can watch English subbed and dubbed anime in high-definition (HD) online.',
+    url: 'https://manualanimeflix.adgstudios.co.za',
+    image: '/AnimeflixSEO.jpeg',
+    socials: {
+      instagram: 'https://www.instagram.com/adgsenpai',
+      github: 'https://github.com/ADGSTUDIOS',
+
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {      
+      logo: true,
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true
+    },
+    footer: {
+      credits: {
+        icon: '',
+        text: 'Made with ❤️ by ADGSTUDIOS',
+        href: 'https://adgstudios.co.za',
+      },
+
+
+    }
+
+  }
+})
---
+++ components/EmbedSocial.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="embedsocial-hashtag" :data-ref="dataRef">
+    <a class="feed-powered-by-es feed-powered-by-es-feed-img" href="https://embedsocial.com/social-media-aggregator/" target="_blank" title="Instagram widget">
+      <img src="https://embedsocial.com/cdn/images/embedsocial-icon.png" alt="EmbedSocial">
+      Instagram widget
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmbedSocialWidget',
+  props: {
+    dataRef: {
+      type: String,
+      required: true,
+    },
+  },
+  mounted() {
+    this.loadScript();
+  },
+  methods: {
+    loadScript() {
+      const scriptId = 'EmbedSocialHashtagScript';
+      if (document.getElementById(scriptId)) return;
+
+      const script = document.createElement('script');
+      script.id = scriptId;
+      script.src = 'https://embedsocial.com/cdn/ht.js';
+      document.getElementsByTagName('head')[0].appendChild(script);
+    },
+  },
+};
+</script>
