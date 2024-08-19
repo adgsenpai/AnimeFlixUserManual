164ee6341828e81c8a776db8c85fb47749dad64d
Ashlin Darius Govindasamy <adg@adgstudios.co.za>
2024-08-20 00:50:30 +0200

    Added Ads

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,27 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: '@nuxt-themes/docus',
   devtools: { enabled: true },
-  
 
   modules: [
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible'
-  ]
+  ],
+
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9261174368196541',
+          async: true,
+          crossorigin: 'anonymous'
+        },
+        {
+          src: '//imamictra.com/rT7M9xuQZdU5/67357',
+          async: true,
+          crossorigin: 'anonymous'
+        },
+      ]
+    }
+  }
 })
