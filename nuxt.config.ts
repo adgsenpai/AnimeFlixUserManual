c76540bc2dee805d707892805f508a60a9b1e7e0
Ashlin Darius Govindasamy <adg@adgstudios.co.za>
2024-08-20 21:04:55 +0200

    Patch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,11 +13,6 @@ export default defineNuxtConfig({
     head: {
       script: [
         {
-          src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9261174368196541',
-          async: true,
-          crossorigin: 'anonymous'
-        },
-        {
           src: '//imamictra.com/rT7M9xuQZdU5/67357',
           async: true,
           crossorigin: 'anonymous'
